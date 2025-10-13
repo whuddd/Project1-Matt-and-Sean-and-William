e778c9d5d88a0e57c33e59c30da679d5afc59674
whuddd <whudd@umich.edu>
2025-10-13 13:28:47 -0400

    added read_penguin_data

---
+++ project1code.py
@@ -0,0 +1,9 @@
+import csv
+
+def read_penguin_data(filename):
+    data = []
+    with open(filename, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            data.append(row)
+    return data
